7b85fb42fb5fbad3f5a355d708998bccacccd319
Shadowfax Bread <shadowfa@student.21-school.ru>
2023-12-17 12:26:11 +0300

    working 3 windows that are connected

--- main.c
+++ main.c
@@ -34,6 +34,8 @@ static void on_warehouse_button_clicked(GtkWidget *widget, gpointer data);
 // static void choose_subsystem_window(GtkApplication *app, gpointer user_data, int employee);
 static void choose_subsystem_window(GtkApplication *app, gpointer user_data);
 static void on_subsystem_button_clicked(GtkWidget *widget, gpointer user_data);
+static void on_staff_button_clicked(GtkWidget *widget, gpointer window);
+static void choose_action_window(GtkApplication *app, gpointer user_data);
 
 // Объявляем структуру, которая будет хранить данные
 // В структуре AppData добавьте указатель на grid
@@ -74,7 +76,12 @@ static void on_auth_button_clicked(GtkWidget *widget, gpointer user_data) {
             g_application_quit(G_APPLICATION(data->app));  // Закрываем приложение
             GtkApplication *app;
             app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-            g_signal_connect(app, "activate", G_CALLBACK(choose_subsystem_window), NULL);
+            if (employee == 1) {
+                g_signal_connect(app, "activate", G_CALLBACK(choose_subsystem_window), NULL);
+            }
+            else {
+                g_signal_connect(app, "activate", G_CALLBACK(choose_action_window), NULL);
+            }
             g_application_run(G_APPLICATION(app), 0, 0);
             g_object_unref(app);
         } 
@@ -82,16 +89,52 @@ static void on_auth_button_clicked(GtkWidget *widget, gpointer user_data) {
     g_slice_free(AppData, data); // Освобождаем память
 }
 
-static void on_subsystem_button_clicked(GtkWidget *widget, gpointer window) {
+static void on_subsystem_button_clicked(GtkWidget *widget, gpointer user_data) {
+    AppData *data = (AppData*)user_data;
+    char* subsystem = NULL;
+    
+    if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(personnel_radio_button))) {
+        g_application_quit(G_APPLICATION(data->app));  // Закрываем приложение
+        GtkApplication *app;
+        app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+        
+        g_signal_connect(app, "activate", G_CALLBACK(choose_action_window), NULL);
+        
+        g_application_run(G_APPLICATION(app), 0, 0);
+        g_object_unref(app);
+         
+    }
+    g_slice_free(AppData, data); // Освобождаем память
+}
+
+
+// static void on_subsystem_button_clicked(GtkWidget *widget, gpointer window) {
+//     // AppData *data = (AppData*)user_data;
+//     if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(personnel_radio_button))) {
+//         g_print("Выбран вариант: Персонал\n");
+//         // g_application_quit(G_APPLICATION(data->app));  // Закрываем приложение
+//     } else if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(orders_radio_button))) {
+//         g_print("Выбран вариант: Заказы\n");
+//         // g_application_quit(G_APPLICATION(data->app));  // Закрываем приложение
+//     } else if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(warehouse_radio_button))) {
+//         g_print("Выбран вариант: Склад\n");
+//         // g_application_quit(G_APPLICATION(data->app));  // Закрываем приложение
+//     }
+//     // g_object_unref(app);
+//     // g_application_quit(G_APPLICATION(data->app));  // Закрываем приложение
+//     // g_slice_free(AppData, data); // Освобождаем память
+// }
+
+static void on_staff_button_clicked(GtkWidget *widget, gpointer window) {
     // AppData *data = (AppData*)user_data;
     if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(personnel_radio_button))) {
-        g_print("Выбран вариант: Персонал\n");
+        g_print("Выбран вариант: Показать\n");
         // g_application_quit(G_APPLICATION(data->app));  // Закрываем приложение
     } else if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(orders_radio_button))) {
-        g_print("Выбран вариант: Заказы\n");
+        g_print("Выбран вариант: Добавить\n");
         // g_application_quit(G_APPLICATION(data->app));  // Закрываем приложение
     } else if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(warehouse_radio_button))) {
-        g_print("Выбран вариант: Склад\n");
+        g_print("Выбран вариант: Завершить\n");
         // g_application_quit(G_APPLICATION(data->app));  // Закрываем приложение
     }
     // g_object_unref(app);
@@ -109,7 +152,6 @@ static void on_warehouse_button_clicked(GtkWidget *widget, gpointer data) {
     // Логика для выбора подсистемы "Warehouse"
 }
 
-// Функция для создания графического интерфейса и добавления элементов
 // Функция для создания графического интерфейса и добавления элементов
 static void activate(GtkApplication *app, gpointer user_data) {
     GtkWidget *window;
@@ -182,7 +224,7 @@ static void choose_subsystem_window(GtkApplication *app, gpointer user_data) {
     grid = gtk_grid_new();
     gtk_container_add(GTK_CONTAINER(window), grid);
 
-    label = gtk_label_new("Выберите один из вариантов:");
+    label = gtk_label_new("Выберите подсистему для работы:");
     gtk_grid_attach(GTK_GRID(grid), label, 0, 0, 1, 1);
 
     // Добавляем радиокнопки для вариантов ответа
@@ -201,6 +243,40 @@ static void choose_subsystem_window(GtkApplication *app, gpointer user_data) {
 
     gtk_widget_show_all(window);
 }
+
+static void choose_action_window(GtkApplication *app, gpointer user_data) {
+    GtkWidget *window;
+    GtkWidget *grid;
+    GtkWidget *label;
+    GtkWidget *button_next;
+
+    window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Welcome to AsuPpg!");
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 800);
+
+    grid = gtk_grid_new();
+    gtk_container_add(GTK_CONTAINER(window), grid);
+
+    label = gtk_label_new("Выберите действие:");
+    gtk_grid_attach(GTK_GRID(grid), label, 0, 0, 1, 1);
+
+    // Добавляем радиокнопки для вариантов ответа
+    personnel_radio_button = gtk_radio_button_new_with_label(NULL, "Показать данные");
+    gtk_grid_attach(GTK_GRID(grid), personnel_radio_button, 0, 1, 1, 1);
+
+    orders_radio_button = gtk_radio_button_new_with_label_from_widget(GTK_RADIO_BUTTON(personnel_radio_button), "Добавить данные");
+    gtk_grid_attach(GTK_GRID(grid), orders_radio_button, 0, 2, 1, 1);
+
+    warehouse_radio_button = gtk_radio_button_new_with_label_from_widget(GTK_RADIO_BUTTON(personnel_radio_button), "Завершить");
+    gtk_grid_attach(GTK_GRID(grid), warehouse_radio_button, 0, 3, 1, 1);
+
+    button_next = gtk_button_new_with_label("Далее");
+    g_signal_connect(button_next, "clicked", G_CALLBACK(on_staff_button_clicked), window);
+    gtk_grid_attach(GTK_GRID(grid), button_next, 0, 4, 1, 1); // Сохраняем координаты как 0, 4, 1, 1
+
+    gtk_widget_show_all(window);
+}
+
 
 
 int proccess(int employee) {
